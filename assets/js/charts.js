cd3a1e12dd578867528a5de589169ed30ad7244e
Matt Hull <matt.hull@lacunasolutionstn.com>
2013-12-23 11:03:46 -0600

    using custom topo json

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,78 +1,58 @@
 var usMap = dc.geoChoroplethChart('#us-map');
 
-//d3.csv('Hospital_Data.csv', function (hospitals) {
-    //window.hospitals = hospitals.map(function (h) {
-        //fullAddress = [h['Address 1']];
-        //if (!!h['Address 2']) fullAddress.push(h['Address 2']);
-        //fullAddress.push([ h['City'], h['State'], h['ZIP Code'] ]);
-        //h['Full Address'] = fullAddress.join(',+')
-        //h['Full Address'] = h['Full Address'].replace(' ', '+');
-        //return h;
-    //});
-
-    //hospitals = hospitals.map(function (h) {
-
-        //d3.json('http://www.mapquestapi.com/geocoding/v1/address?key=Fmjtd%7Cluubn10anq%2C2s%3Do5-90bwgf&location=1555 Blake St,Denver,CO,80202', function (resp) {
-            //h.latLng = resp.results[0].locations[0].latLng;
-            //return h;
-        //});
-    //});
-
 d3.json('hospitals.json', function (hospitals) {
-    window.hospitals = hospitals;
-    //window.hospitals = hospitals.map(function (h) {
-        //d3.json('http://data.fcc.gov/api/block/find?format=json&latitude='+h.latLng.lat+'&longitude='+h.latLng.lng+'&showall=true', function (c) {
-        //return h;
-    //});
-
     var data = crossfilter(hospitals);
 
-    window.stateDim = data.dimension(function (d) {
+    stateDim = data.dimension(function (d) {
         return d.State;
     });
 
-    window.stateGroup = stateDim.group().reduceCount();
+    countyDim = data.dimension( function (d) { return { FIPS: d.censusBlock.County.FIPS, name: d.censusBlock.County.Name } });
 
-    d3.json("us-states.json", function (states) {
-        d3.json("us-counties.json", function (counties) {
+    stateGroup = stateDim.group().reduceCount();
+    countyGroup = countyDim.group().reduceCount();
 
-            usMap.width(960)
-                    .height(500)
-                    .dimension(stateDim)
-                    .group(stateGroup)
-                    .colors(d3.scale.quantize().range(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"]))
-                    .colorDomain([0, 500])
-                    .colorCalculator(function (d) { return d ? usMap.colors()(d) : '#ccc'; })
-                    .projection(d3.geo.albersUsa())
-                    .overlayGeoJson(states.features, "state", function (d) {
-                        return d.properties.name;
-                    });
+    d3.json("us.json", function (us) {
+        //counties = topojson.feature(us, us.objects.counties);
+        states = topojson.feature(us, us.objects.states).features;
 
-            usMap.renderlet(function (chart) {
-                chart.root().call(zoom);
+        usMap
+            .width(960)
+            .height(500)
+            .dimension(stateDim)
+            .group(stateGroup)
+            .colors(d3.scale.quantize().range(["#E2F2FF", "#C4E4FF", "#9ED2FF", "#81C5FF", "#6BBAFF", "#51AEFF", "#36A2FF", "#1E96FF", "#0089FF", "#0061B5"]))
+            .colorDomain([0, 500])
+            .colorCalculator(function (d) { return d ? usMap.colors()(d) : '#ccc'; })
+            .projection(d3.geo.albersUsa())
+            .overlayGeoJson(states, "state", function (d) {
+                return d.properties.name;
             });
 
-            function zoomed() {
-                usMap.getPath().projection().translate(d3.event.translate).scale(d3.event.scale);
-                dc.events.trigger(function () {
-                    usMap.svg().selectAll("g.layer0 path").attr("d", usMap.getPath());
-                    window.visible = states.features.filter(function (s) {
-                        var centroid = usMap.getPath().centroid(s);
-                        return centroid[0] > 0 && centroid[0] < usMap.width() && centroid[1] > 0 && centroid[1] < usMap.height()
-                    }).map(function (s) {return s.properties.name});
-                    var filters = usMap.filters();
-                    filters.length = 0;
-                    visible.forEach(function (s) {
-                        filters.push(s);
-                    });
-                    usMap.redraw();
-                    //TODO: need to make sure 1 state is always selected
-                    //TODO: better line sizes - currently gets thicker as you zoom out
-                }, 5);
-            }
-
-            window.zoom = d3.behavior.zoom().translate(d3.geo.albersUsa().translate()).scale(d3.geo.albersUsa().scale()).on("zoom", zoomed);
-            dc.renderAll();
+        usMap.renderlet(function (chart) {
+            chart.root().call(zoom);
         });
+
+        function zoomed() {
+            usMap.getPath().projection().translate(d3.event.translate).scale(d3.event.scale);
+            usMap.svg().selectAll("g.layer0 path").attr("d", usMap.getPath());
+            dc.events.trigger(function () {
+                window.visible = states.filter(function (s) {
+                    centroid = usMap.getPath().centroid(s);
+                    return centroid[0] > 0 && centroid[0] < usMap.width() && centroid[1] > 0 && centroid[1] < usMap.height();
+                }).map(function (s) {return s.properties.name});
+                var filters = usMap.filters();
+                filters.length = 0;
+                visible.forEach(function (s) {
+                    filters.push(s);
+                });
+                usMap.redraw();
+                //TODO: need to make sure 1 state is always selected
+                //TODO: better line sizes - currently gets thicker as you zoom out
+            }, 25);
+        }
+
+        window.zoom = d3.behavior.zoom().translate(d3.geo.albersUsa().translate()).scale(d3.geo.albersUsa().scale()).on("zoom", zoomed);
+        dc.renderAll();
     });
 });
