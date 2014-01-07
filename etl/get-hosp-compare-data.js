93394a02dc92f79ab85f0d2829d5435415cf1095
Liz Brandon <brandon.elizabeth@gmail.com>
2014-01-06 21:47:57 -0600

    changed to leave out hospitals without census data

--- etl/get-hosp-compare-data.js
+++ etl/get-hosp-compare-data.js
@@ -14,22 +14,24 @@ var db = new Db('hosp', server);
 
 var request = require('request');
 db.open(function(err,db) {
-    var collection = db.collection('hospitalsRaw');
+    var hospitalsCollection = db.collection('hospitalsRaw');
     var rowCount = 0;
     [0,1000,2000,3000,4000,5000].forEach(function (offset) {
+        console.log('retrieving hospitals starting with ' + offset);
         request('http://data.medicare.gov/resource/v287-28n3.json?$$app_token=2I2Z6fMimNMRGfqlwShjSbeCU&$limit=1000&$offset='+offset, function(err, response, body) {
             if (!err && response.statusCode === 200) {
-                collection.insert(JSON.parse(body), function(err, records){console.log(err)});
+                hospitalsCollection.insert(JSON.parse(body), function(err, records){if(err){console.log(err)}});
             }
             else console.log('Status code: ' + response.statusCode, body);
         });
     });
 
-    var collection = db.collection('outcomesRaw');
+    var outcomesCollection = db.collection('outcomesRaw');
     [0, 1000, 2000, 3000, 4000, 5000, 6000].forEach(function (offset) {
+        console.log('retrieving outcomes starting with ' + offset);
         request('http://data.medicare.gov/resource/rcw8-6swd.json?$$app_token=2I2Z6fMimNMRGfqlwShjSbeCU&$offset='+offset, function (err, response, body) {
             if (!err && response.statusCode === 200) {
-                collection.insert(JSON.parse(body), function(err, records){console.log(err)});
+                outcomesCollection.insert(JSON.parse(body), function(err, records){if(err){console.log(err)}});
             }
             else console.log('Status code: ' + response.statusCode, body);
         })
