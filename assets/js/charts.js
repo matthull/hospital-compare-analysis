65ca5beac2e5b6c5d44f1f8e2cd67e4d313cec9f
Matt Hull <matt.hull@lacunasolutionstn.com>
2014-01-06 21:15:42 -0600

    added several charts for hospital types and outcomes

--- assets/js/charts.js
+++ assets/js/charts.js
@@ -1,151 +1,179 @@
 var usMap = dc.geoChoroplethChart('#us-map');
+//var stateOutcomes = dc.rowChart('#state-outcomes');
+var totalHospitals = dc.numberDisplay('#total-hospitals');
+var overallOutcomesScore = dc.numberDisplay('#overall-outcomes-score');
 
-d3.json('hospitals.json', function (hospitals) {
-    d3.csv('outcomes.csv', function (outcomesRaw) {
-        d3.csv('medicare_spending.csv', function (spendingRaw) {
-            function comparisonCodeFor(comparisonDescription) {
-                var comparisonCodes = {
-                    'Better than U.S. National Rate': 'better',
-                    'No Different than U.S. National Rate': 'neutral',
-                    'Worse than U.S. National Rate': 'worse',
-                    'Not Available': 'na',
-                    'Number of Cases Too Small': 'na'
-                };
-                var code = comparisonCodes[comparisonDescription];
-                if (code === undefined) throw 'No code defined for ' + comparisonDescription;
-                return comparisonCodes[comparisonDescription];
+var categoryCounts = dc.pieChart('#category-counts');
+var ownershipCounts = dc.pieChart('#ownership-counts');
+var hasEdCounts = dc.pieChart('#has-ed-counts');
+
+var categoryOutcomes = dc.rowChart('#category-outcomes')
+
+var ndx;
+var totalHospitalsGroup, stateDim, countyDim, stateHospitalsCountGroup, countyHospitalsCountGroup, stateOutcomesGroup, countyOutcomesGroup;
+var categoryDim, ownershipDim, hasEdDim;
+var categoryCountsGroup, categoryOutcomesGroup, ownershipCountsGroup, ownershipOutcomesGroup, hasEdCountsGroup, hasEdOutcomesGroup;
+
+function isna (r) { return r === 'na'; };
+
+function scoreOutcome (o) {
+    scoreMappings = {
+        'same': 1,
+        'above': 2,
+        'below': 0
+    }
+    return scoreMappings[o];
+}
+
+d3.json('/hospitals', function (hospitals) {
+    ndx = crossfilter(hospitals);
+
+    totalHospitalsGroup = ndx.groupAll().reduceCount();
+    overallOutcomesScoreGroup = ndx.groupAll().reduce(outcomesAdd, outcomesRemove, outcomesInit);
+
+    stateDim = ndx.dimension(function (d) { return d.location.state });
+    countyDim = ndx.dimension(function (d) { return d.location.countyFIPS });
+
+    stateHospitalsCountGroup = stateDim.group().reduceCount();
+    countyHospitalsCountGroup = countyDim.group().reduceCount();
+
+    function outcomesAdd (p,v) {
+        rates = v.outcomeUsRateComparisons;
+        if (rates) {
+            scores = _.values(rates.readmissionRates).concat(_.values(rates.mortalityRates));
+            if (scores.length > 0 & !scores.some(isna)) {
+                totalScore = scores.reduce(function (a, b ) {
+                    return scoreOutcome(b) + a;
+                }, 0);
+                p.totalHospitals++;
+                p.totalScore += totalScore;
+                p.averageScore = p.totalScore / p.totalHospitals;
             }
+        }
+        return p;
+    };
+    function outcomesRemove (p,v) {
+        rates = v.outcomeUsRateComparisons;
+        if (rates) {
+            scores = _.values(rates.readmissionRates).concat(_.values(rates.mortalityRates));
+            if (scores.length > 0 & !scores.some(isna)) {
+                totalScore = scores.reduce(function (a, b ) {
+                    return scoreOutcome(b) + a;
+                }, 0);
+                p.totalHospitals--
+                p.totalScore -= totalScore;
+                p.averageScore = p.totalHospitals === 0 ? 0 : p.totalScore / p.totalHospitals;
+            }
+        }
+        return p;
+    };
+    function outcomesInit () {
+        return {
+            totalScore: 0,
+            totalHospitals: 0,
+            averageScore: 0
+        };
+    };
 
-            stats = outcomesRaw.map(function (o) {
-                providers = hospitals.filter(function (h) { return h['Provider Number'] === o['Provider Number']});
-                if (providers[0] === undefined) throw 'Could not lookup provider data for hospital ID ' + o['Provider Number'];
-                if (providers.length > 1) throw 'Found ' + providers.length + ' hospitals for hospital ID ' + o['Provider Number'];
-                provider = providers[0];
+    stateOutcomesGroup = stateDim.group().reduce(outcomesAdd, outcomesRemove, outcomesInit);
 
-                spendings = spendingRaw.filter(function (s) { return s['Provider ID'] === o['Provider Number']});
-                if (spendings.length > 1) throw 'Found ' + spendings.length + ' spending datas for hospital ID ' + o['Provider Number'];
-                spending = spendings[0];
+    categoryDim = ndx.dimension(function (d) { return d.category; });
+    categoryCountsGroup = categoryDim.group().reduceCount();
+    categoryOutcomesGroup = categoryDim.group().reduce(outcomesAdd, outcomesRemove, outcomesInit);
 
-                return {
-                    provider: {
-                        id: o['Provider Number'],
-                        county: {
-                            FIPS: +provider.censusData.County.FIPS,
-                            name: provider.censusData.County.name
-                        },
-                        state: {
-                            code: provider.censusData.State.code
-                        }
-                    },
-                    spendingPerBeneficiary: spending ? spending['Spending per Hospital Patient with Medicare'] : undefined,
-                    comparativeRatings: {
-                        mortality: {
-                            heartAttack: comparisonCodeFor(o['Comparison to U S  Rate - Hospital 30-Day Death (Mortality) Rates from Heart Attack']),
-                            heartFailure: comparisonCodeFor(o['Comparison to U S  Rate - Hospital 30-Day Death (Mortality) Rates from Heart Failure']),
-                            pneumonia: comparisonCodeFor(o['Comparison to U S  Rate - Hospital 30-Day Death (Mortality) Rates from Pneumonia'])
-                        },
-                        readmission: {
-                            heartAttack: comparisonCodeFor(o['Comparison to U S  Rate - Hospital 30-Day Readmission Rates from Heart Attack']),
-                            heartFailure: comparisonCodeFor(o['Comparison to U S  Rate - Hospital 30-Day Readmission Rates from Heart Failure']),
-                            pneumonia: comparisonCodeFor(o['Comparison to U S  Rate - Hospital 30-Day Readmission Rates from Pneumonia']),
-                            overall: comparisonCodeFor(o['All cause hospital-wide readmission'])
-                        },
-                        kneeSurgeryComplicationsAndDeaths: comparisonCodeFor(o['Complications and Deaths following hip/knee surgery'])
-                    }
-                };
-            });
+    ownershipDim = ndx.dimension(function (d) { return d.ownership; });
+    ownershipCountsGroup = ownershipDim.group().reduceCount();
+    ownershipOutcomesGroup = ownershipDim.group().reduce(outcomesAdd, outcomesRemove, outcomesInit);
 
-            var ndx = crossfilter(stats);
+    hasEdDim = ndx.dimension(function (d) { return d.hasEmergencyServices; });
+    hasEdCountsGroup = hasEdDim.group().reduceCount();
+    hasEdOutcomesGroup = hasEdDim.group().reduce(outcomesAdd, outcomesRemove, outcomesInit);
 
-            var stateDim = ndx.dimension(function (d) { return d.provider.state.code });
-            var countyDim = ndx.dimension(function (d) { return d.provider.county.FIPS });
+    d3.json("us.json", function (us) {
+        var countiesMap = topojson.feature(us, us.objects.counties).features;
+        var statesMap = topojson.feature(us, us.objects.states).features;
 
-            var stateHospitalsCountGroup = stateDim.group().reduceCount();
-            var stateQualityOfCare = stateDim.group().reduce(
-                function (p,v) {
+        function setupMapColors (chart) {
+            var colorScale = d3.scale.quantize().range(colorbrewer.Greens[9].slice(2,8)).domain(d3.extent(_.pluck(chart.group().all(), 'value')));
+            chart.colors(colorScale);
+            d3.select('#map-legend').selectAll('svg').remove().transition();
+            colorlegend('#map-legend', usMap.colors(), 'quantile', { title: "# of Hospitals", boxHeight: 15, boxWidth: 30 });
+        };
 
-                },
-                function (p,v) {
-                },
-                function () {
-                    return {
+        usMap
+            .width(480)
+            .height(250)
+            .dimension(stateDim)
+            .group(stateHospitalsCountGroup, '# of Hospitals')
+            .overlayGeoJson(statesMap, "state", function (d) {
+                return d.properties.name;
+            })
+            .dimension(countyDim)
+            //.group(countyHospitalsCountGroup, '# of Hospitals')
+            //.overlayGeoJson(countiesMap, "admin5", function (d) {
+                //return d.id.toString();
+            //})
+            .projection(d3.geo.albersUsa().scale(535).translate([240,125]))
+            .on('preRender', setupMapColors)
+            .on('preRedraw', setupMapColors);
 
-                    };
-                }
-            );
-            //var stateMedicareSpendingGroup = stateDim.group().reduce(
-                //function (p,v) {
-                    //p.hospitals++;
-                    //p.total += v.spendingPerBeneficiary;
-                    //p.avgPerHospital = p.total / p.hospitals;
-                    //return p;
-                //},
-                //function (p,v) {
-                    //p.hospitals--
-                    //p.total -= v.spendingPerBeneficiary;
-                    //p.avgPerHospital = p.total / p.hospitals;
-                    //if (p.hospitals === 0) p.avgPerHospital = 0;
-                    //return p;
+        function zoomed() {
+            usMap.getPath().projection().translate(d3.event.translate).scale(d3.event.scale);
+            usMap.svg().selectAll("g.layer0 path").attr("d", usMap.getPath()).style("stroke-width", "1.5px");
+            dc.events.trigger(function () {
+                visible = statesMap.filter(function (s) {
+                    centroid = usMap.getPath().centroid(s);
+                    return centroid[0] > 0 && centroid[0] < usMap.width() && centroid[1] > 0 && centroid[1] < usMap.height();
+                }).map(function (s) {return s.properties.name});
+                var filters = usMap.filters();
+                filters.length = 0;
+                visible.forEach(function (s) { filters.push(s) });
+                usMap.redraw();
+                //TODO: need to make sure 1 state is always selected
+                //TODO: need to handle user selection/deselection combined with zoom
+            }, 25);
+        }
 
-                //},
-                //function () {
-                    //return {
-                        //hospitals: 0,
-                        //total: 0,
-                        //avgPerHospital: 0
-                    //}
-                //}
-            //)
-            var countyHospitalsCountGroup = countyDim.group().reduceCount();
+        var zoom = d3.behavior.zoom().translate(usMap.getPath().projection().translate()).scale(usMap.getPath().projection().scale()).on("zoom", zoomed);
+        usMap.root().call(zoom);
 
-            d3.json("us.json", function (us) {
-                var countiesMap = topojson.feature(us, us.objects.counties).features;
-                var statesMap = topojson.feature(us, us.objects.states).features;
-                var colorScale = d3.scale.quantize().range(colorbrewer.Greens[9].slice(2,8)).domain(d3.extent(_.pluck(stateHospitalsCountGroup.all(), 'value')));
-                //var colorScale = d3.scale.quantize().range(colorbrewer.Greens[9].slice(2,8)).domain(d3.extent(_.pluck(countyHospitalsCountGroup.all(), 'value')));
+        totalHospitals.group(totalHospitalsGroup).valueAccessor(function (d) { return d; }).formatNumber(d3.format('.0f'));
+        overallOutcomesScore.group(overallOutcomesScoreGroup).valueAccessor(function (d) { return d.averageScore; }).formatNumber(d3.format('.2f'));
 
-                usMap
-                    .width(480)
-                    .height(250)
-                    .dimension(stateDim)
-                    .group(stateHospitalsCountGroup, '# of Hospitals')
-                    .overlayGeoJson(statesMap, "state", function (d) {
-                        return d.properties.name;
-                    })
-                    //.dimension(countyDim)
-                    //.group(countyHospitalsCountGroup, '# of Hospitals')
-                    //.overlayGeoJson(countiesMap, "admin5", function (d) {
-                        //return d.id.toString();
-                    //})
-                    .colors(colorScale)
-                    .projection(d3.geo.albersUsa().scale(535).translate([240,125]));
+        //stateOutcomes
+            //.width(false)
+            //.height(false)
+            //.dimension(stateDim)
+            //.group(stateOutcomesGroup)
+            //.valueAccessor()
 
-                colorlegend('#map-legend', colorScale, 'quantile', { title: "# of Hospitals", boxHeight: 15, boxWidth: 30 });
+        categoryCounts
+            .width(false)
+            .height(false)
+            .dimension(categoryDim)
+            .group(categoryCountsGroup);
 
-                function zoomed() {
-                    usMap.getPath().projection().translate(d3.event.translate).scale(d3.event.scale);
-                    usMap.svg().selectAll("g.layer0 path").attr("d", usMap.getPath()).style("stroke-width", "1.5px");
-                    dc.events.trigger(function () {
-                        visible = statesMap.filter(function (s) {
-                            centroid = usMap.getPath().centroid(s);
-                            return centroid[0] > 0 && centroid[0] < usMap.width() && centroid[1] > 0 && centroid[1] < usMap.height();
-                        }).map(function (s) {return s.properties.name});
-                        var filters = usMap.filters();
-                        filters.length = 0;
-                        visible.forEach(function (s) { filters.push(s) });
-                        usMap.redraw();
-                        //TODO: need to make sure 1 state is always selected
-                    }, 25);
-                }
+        ownershipCounts
+            .width(false)
+            .height(false)
+            .dimension(ownershipDim)
+            .group(ownershipCountsGroup);
 
-                var zoom = d3.behavior.zoom().translate(usMap.getPath().projection().translate()).scale(usMap.getPath().projection().scale()).on("zoom", zoomed);
-                usMap.root().call(zoom);
-                usMap.renderlet(function (chart) {
+        hasEdCounts
+            .width(false)
+            .height(false)
+            .dimension(hasEdDim)
+            .group(hasEdCountsGroup);
 
-                });
+        categoryOutcomes
+            .width(false)
+            .height(false)
+            .dimension(categoryDim)
+            .group(categoryOutcomesGroup)
+            .valueAccessor(function (d) { return d.value.averageScore; })
+            .x(d3.scale.linear().domain(d3.extent(_.pluck(_.pluck(categoryOutcomesGroup.all(), 'value'), 'averageScore'))))
+            .elasticX(true)
 
-                dc.renderAll();
-            });
-        });
+        dc.renderAll();
     });
 });
